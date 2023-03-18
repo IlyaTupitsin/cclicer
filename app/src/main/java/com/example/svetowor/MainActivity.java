1d2690a86bae246c69f61adce2dd300a88897701
Ilya <tupitsin2004@mail.ru>
2023-03-18 14:25:07 +0300

    светофор

---
+++ app/src/main/java/com/example/svetowor/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.svetowor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button red;
+    private Button yellow;
+    private Button green;
+    private View view;
+    private int currentBackgroundColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        red = findViewById(R.id.red_button);
+        yellow = findViewById(R.id.yellow_button);
+        green = findViewById(R.id.green_button);
+        view = findViewById(R.id.activity);
+
+
+        red.setOnClickListener(this);
+        yellow.setOnClickListener(this);
+        green.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        currentBackgroundColor = Color.TRANSPARENT;
+        Drawable background = view.getBackground();
+        if (background instanceof ColorDrawable)
+            currentBackgroundColor = ((ColorDrawable) background).getColor();
+
+        outState.putInt("background_color", currentBackgroundColor);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (savedInstanceState != null && savedInstanceState.containsKey("background_color")) {
+            view.setBackgroundColor(savedInstanceState.getInt("background_color"));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.red_button:
+                view.setBackgroundColor(Color.RED);
+                break;
+            case R.id.yellow_button:
+                view.setBackgroundColor(Color.YELLOW);
+                break;
+            case R.id.green_button:
+                view.setBackgroundColor(Color.GREEN);
+                break;
+        }
+    }
+}
